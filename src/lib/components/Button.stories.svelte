482a1a8fdb20ea91fa2173b012eed5c31f93a844
Giuseppe Caruso <giuseppe.caruso@grid.app>
2022-08-11 15:33:10 +0200

    add a native svelte story

---
+++ src/lib/components/Button.stories.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+
+	import Button from '$lib/components/Button.svelte';
+
+	let count = 0;
+
+	function handleClick() {
+		count += 1;
+	}
+</script>
+
+<Meta title="Native Stories/Button" component={Button} />
+
+<Template let:args>
+	<Button {...args} on:click={handleClick}>
+		You clicked: {count}
+	</Button>
+</Template>
+
+<Story name="Native Story Button" />
+
+<!-- Dynamic snippet should be disabled for this story -->
+<!-- <Story name="Button No Args">
+     <Button>Label</Button>
+     </Story> -->
