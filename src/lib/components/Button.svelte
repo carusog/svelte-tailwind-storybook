e5eec715ddcecad212c9a68343ecc6cbb4e151e4
Giuseppe Caruso <giuseppe.caruso@grid.app>
2022-08-11 15:32:38 +0200

    add and implement a simple button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	export let text = '';
+	const dispatch = createEventDispatcher();
+
+	function onClick(event: MouseEvent): void {
+		dispatch('click', event);
+	}
+</script>
+
+<button class="button" on:click={onClick}>
+	{text}
+	<slot />
+</button>
+
+<style lang="postcss">
+	.button {
+		@apply ease-in duration-300 rounded-full px-4 py-2 text-white bg-cyan-700 border-4 border-cyan-600 hover:outline outline-4 outline-cyan-500;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,11 @@
+<script>
+	import Button from '$lib/components/Button.svelte';
+</script>
+
 <h1 class="text-red-600 font-extrabold text-6xl">Welcome to SvelteKit</h1>
 <h2 class="text-cyan-600 font-bold text-3xl">
 	This is an attempt to make SvelteKit, Tailwind and StoryBook work together
 </h2>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Button text="Click me!" />
